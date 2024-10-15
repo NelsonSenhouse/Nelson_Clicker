a1dfa779d926b433adda54bf93684d18d0b36694
NelsonSenhouse <144487032+NelsonSenhouse@users.noreply.github.com>
2024-10-15 15:34:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Title: Build a Counter App
 var num = 0
-var button = document.getElementById("count-el")
-button.innerHTML = num
+var button1 = document.getElementById("count-el")
+button2.innerHTML = num
+var button2 = document.getElementById("save-btn")
 
 function increment() {
     num ++
-    button.innerHTML = num
+    button1.innerHTML = num
     console.log(num)
+}
+
+function save() {
+    document.getElementById("entries").innerHTML += num + " - "
 }
